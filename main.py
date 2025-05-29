5b0a7bba2d8d8f4966901920475ab11e7f400edd
bradley-kyan <bbradley.k1@gmail.com>
2025-05-29 13:03:24 +1200

    Ruff format

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ totp_auth_factory = totp_factory(totp_storeage_filename="totp_backup.txt")
 
 example_auth = totp_auth_factory.create_totp_instance(identifier="example_user")
 
+
 def run_totp_cycle():
     while True:
         print("Generating TOTP code...")
         totp_code = example_auth.generate_totp()
         print(f"TOTP Code: {totp_code}")
-        
+
         time.sleep(10)
-        
+
+
 run_totp_cycle()
